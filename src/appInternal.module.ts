315c41b16cc39bbc290d45f551efc28e7556fea4
new-thanh <new-thanh@eplus.vn>
2021-01-01 12:06:48 +0700

    change app internal module

--- src/appInternal.module.ts
+++ src/appInternal.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
-import { PostsModule } from './posts/posts.module';
-import { CategoriesModule } from './categories/categories.module';
-import { CommentsModule } from './comments/comments.module';
-import { UsersModule } from './users/users.module';
 import { AppInternalController } from './appInternal.controller';
 import { AppInternalService } from './appInternal.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/blog'),
-    PostsModule,
-    CategoriesModule,
-    CommentsModule,
-    UsersModule,
   ],
   controllers: [AppInternalController],
   providers: [AppInternalService],
